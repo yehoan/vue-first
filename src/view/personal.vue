aedce5be32428355079599768265bed080c42a98
yesnamelukas <yesnamelukas@gmail.com>
2020-01-17 21:35:25 +0800

    OT-100 #个人中心

--- src/view/personal.vue
+++ src/view/personal.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        这是个人中心
+        {{titleName}}
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            titleName: '这是个人中心'
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
